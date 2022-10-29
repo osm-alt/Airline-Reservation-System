c6cec1608d707d7dc29b3fef85050535918235c2
AndrewChemaly <andrew.chemaly@gmail.com>
2022-10-29 20:32:31 +0300

    changes to all my pages and passenger services

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-
 
 function myFunction() {
     var x = document.getElementById("myLinks");
